67e5f4c66938995cbacbe3155aa6cdc9b3802382
Bassanelli Simone <bassanelli.simone.studente@itispaleocapa.it>
2023-10-17 13:09:06 +0200

    Funzione  velocità

--- Automobile/Class1.cs
+++ Automobile/Class1.cs
@@ -1,20 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Automobile
 {
-    public class automobile 
+    public class Automobile 
     {
-        protected int _Velocità;
-        protected bool _Acceso;
-        protected int _Marcia;
-    }
+        protected int _velocità;
+        protected bool _acceso;
+        protected int _marcia;
 
-    public automobile (int _velocità, bool _acceso, int _marcia);
-    {
+        public Automobile (int Velocità, bool Acceso, int Marcia)
+        {
+            _velocità = Velocità;
+            _acceso = false;
+            _marcia = Marcia;
+        }
 
-    }
+        public void AccendiMotore()
+        {
+            _acceso = true;
+        }
+
+        public void SpegniMotore()
+        {
+            if (_velocità <= 0) 
+            {
+                _acceso= false; 
+            }
+            else
+            {
+                _acceso = true;
+            }
+        }
+
+        public void Accellerare(int Velocità)
+        {
+            if (_acceso = true && _marcia < 5)
+            {
+                Velocità += 10;
+            }
+        }
+
+        public void Deccellerare(int Velocità)
+        {
+            if (_acceso = true && _marcia > 0)
+            {
+                Velocità -= 10;
+            }
+        }
+
+
+
+    } 
 }
